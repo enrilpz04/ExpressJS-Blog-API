f987b4d3082abd3ed3183b2f0691f460ed6317e9
enrilpz04 <enriquelopezgordillo@gmail.com>
2024-11-06 19:35:20 +0100

    routes defined

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
 
+router.use('/authors', require('./api/authors.routes'));
+router.use('/posts', require('./api/posts.routes'));
+
 module.exports = router;
